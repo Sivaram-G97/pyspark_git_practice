c2440afaad7b9f05803ce5bba57bf7f18a2fbb44
Sivaram G <sivaram.gnanavel@gmail.com>
2026-03-19 17:18:13 +0530

    practice commit successful

---
+++ git_practice_python.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName("GitPractice").getOrCreate()
+data = [("Alice", 30), ("Bob", 25), ("Charlie", 35)]
+schema = StructType([
+    StructField("name", StringType(), True),
+    StructField("age", IntegerType(), True)
+])
+df = spark.createDataFrame(data, schema)
